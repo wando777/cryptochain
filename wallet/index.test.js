9e819ffecf0db62fd33492187ee668872a31c2ce
Wanderson Herênio Aragão Leite <wando@Wandersons-MBP.local>
2021-07-19 23:20:33 -0300

    wallet v1.2

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
     let wallet;
@@ -15,5 +16,31 @@ describe('Wallet', () => {
         console.log(wallet.publicKey);
         expect(wallet).toHaveProperty('publicKey');
     });
+
+    describe('signin data', () => {
+        const data = 'foobar';
+
+        it('verifies a signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: wallet.sign(data)
+                })
+            ).toBe(true);
+        });
+
+        it('does not verify an invalid signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: new Wallet().sign(data)
+                })
+            ).toBe(false);
+        });
+
+    });
+
 
 });
